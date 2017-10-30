22b4ac953d2401057cd0433fc809c3e06208f147
Sod-Momas <sothereer@gmail.com>
2017-10-31 00:32:33 +0800

    first commit

---
+++ src/cc/momas/car/web/action/BaseAction.java
@@ -0,0 +1,9 @@
+package cc.momas.car.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+}
